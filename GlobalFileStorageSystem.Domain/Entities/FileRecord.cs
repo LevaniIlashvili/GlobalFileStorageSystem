c5a55ea90a8bfa17161aa709555a5d36e7df7882
Levan Ilashvili <levanilashvili2005@gmail.com>
2025-07-15 17:36:32 +0400

    feat: create file entity

---
+++ GlobalFileStorageSystem.Domain/Entities/FileRecord.cs
@@ -0,0 +1,34 @@
+﻿using GlobalFileStorageSystem.Domain.Enums;
+
+namespace GlobalFileStorageSystem.Domain.Entities
+{
+    public class FileRecord : BaseEntity
+    {
+        public Guid TenantId { get; set; }
+
+        public string FileName { get; set; }
+        public long FileSize { get; set; } // in bytes
+        public string ContentType { get; set; }
+        public string StoragePath  { get; set; }
+
+        public string MD5Hash { get; set; }
+        public string SHA256Hash { get; set; }
+        public string? EncryptionKeyId { get; set; }
+
+        public DateTime UploadTimestamp { get; set; }
+        public DateTime? LastAccessedTimestamp { get; set; }
+        public DateTime? ExpirationDate { get; set; }
+
+        public int VersionNumber { get; set; }
+        public Dictionary<string, string> Metadata { get; set; }
+        public List<string> Tags { get; set; }
+        public AccessLevel AccessLevel { get; set; }
+
+        public Guid UploadedBy { get; set; }
+    
+        public bool IsRestricted => AccessLevel == AccessLevel.Restricted;
+
+        public User User { get; set; }
+        public Tenant Tenant { get; set; }
+    }
+}

e0dfe6ca98fe391c69a8999f1295845914556ab7
Levan Ilashvili <levanilashvili2005@gmail.com>
2025-07-15 17:36:43 +0400

    feat: create usage record entity

---
+++ GlobalFileStorageSystem.Domain/Entities/UsageRecord.cs
@@ -0,0 +1,18 @@
+﻿namespace GlobalFileStorageSystem.Domain.Entities
+{
+    public class UsageRecord : BaseEntity
+    {
+        public Guid TenantId { get; set; }
+
+        public long StorageUsed { get; set; } // Bytes
+        public long BandwidthUsed { get; set; } // Bytes
+        public int APICallsCount { get; set; }
+        public long FileOperationCount { get; set; }
+        public int ActiveUserCount { get; set; }
+        public long StorageTransactions { get; set; } // Total GET/PUT/DELETE 
+
+        public DateTime SnapshotDate { get; set; } // month
+
+        public Tenant Tenant { get; set; }
+    }
+}

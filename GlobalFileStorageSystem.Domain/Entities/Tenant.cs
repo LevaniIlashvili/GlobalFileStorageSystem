5e2ad455699187300204eefa773f71de067fdf86
Levan Ilashvili <levanilashvili2005@gmail.com>
2025-07-15 17:36:06 +0400

    feat: create Tenant entity

---
+++ GlobalFileStorageSystem.Domain/Entities/Tenant.cs
@@ -0,0 +1,22 @@
+﻿using GlobalFileStorageSystem.Domain.Enums;
+
+namespace GlobalFileStorageSystem.Domain.Entities
+{
+    public class Tenant : BaseEntity
+    {
+        public string OrganizationName { get; set; }
+        public string SubdomainPrefix { get; set; }
+        public TenantStatus TenantStatus { get; set; }
+        public BillingPlan BillingPlan { get; set; }
+        public long StorageQuota { get; set; } // in bytes
+        public long BandwithQuota { get; set; } // in bytes
+        public int APIRateLimit { get; set; } // requests per minute
+        public string DataResidencyRegion { get; set; }
+        public ComplianceRequirement ComplianceRequirements { get; set; }
+        public EncriptionRequirement EncryptionRequirements { get; set; }
+
+        public List<User> Users { get; set; }
+        public List<FileRecord> Files { get; set; }
+        public List<UsageRecord> Usages { get; set; }
+    }
+}

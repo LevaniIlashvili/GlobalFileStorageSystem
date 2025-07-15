6664cb66fc1e0125a7e7251e3b7c64be841af0bb
Levan Ilashvili <levanilashvili2005@gmail.com>
2025-07-15 18:04:02 +0400

    feat: create tenant repository

---
+++ GlobalFileStorageSystem.Infrastructure/Repositories/TenantRepository.cs
@@ -0,0 +1,11 @@
+﻿using GlobalFileStorageSystem.Application.Contracts.Infrastructure;
+using GlobalFileStorageSystem.Domain.Entities;
+using GlobalFileStorageSystem.Infrastructure.Persistance;
+
+namespace GlobalFileStorageSystem.Infrastructure.Repositories
+{
+    public class TenantRepository : BaseRepository<Tenant>, ITenantRepository
+    {
+        public TenantRepository(AppDbContext dbContext) : base(dbContext) { }
+    }
+}

9d3428c8d9ec99e5a09715a0771c992ae3164759
Levan Ilashvili <levanilashvili2005@gmail.com>
2025-07-15 17:36:21 +0400

    feat: create user entity

---
+++ GlobalFileStorageSystem.Domain/Entities/User.cs
@@ -0,0 +1,24 @@
+﻿using GlobalFileStorageSystem.Domain.Enums;
+
+namespace GlobalFileStorageSystem.Domain.Entities
+{
+    public class User : BaseEntity
+    {
+        public Guid TenantId { get; set; }
+
+        public string Email { get; set; }
+        public UserRole Role { get; set; }
+        public Permission Permissions { get; set; }
+
+        public DateTime? LastLoginTimestamp { get; set; }
+        public bool MFAEnabled { get; set; }
+        
+        public string? APIKeyHash { get; set; }
+        public TimeSpan SessionTimeout { get; set; }
+        public List<string>? IPWhitelist { get; set; }
+
+        public Tenant Tenant { get; set; }
+        public List<FileRecord> Files { get; set; }
+    }
+}
+ 

a34f8b72d1e6ba89077056e80befe139839346da
Levan Ilashvili <levanilashvili2005@gmail.com>
2025-07-15 17:38:14 +0400

    feat: create db context

---
+++ GlobalFileStorageSystem.Infrastructure/Persistance/AppDbContext.cs
@@ -0,0 +1,20 @@
+﻿using GlobalFileStorageSystem.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace GlobalFileStorageSystem.Infrastructure.Persistance
+{
+    public class AppDbContext : DbContext
+    {
+        public DbSet<Tenant> Tenants => Set<Tenant>();
+        public DbSet<FileRecord> Files => Set<FileRecord>();
+        public DbSet<User> Users => Set<User>();
+        public DbSet<UsageRecord> UsageRecords => Set<UsageRecord>();
+
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options) { }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.ApplyConfigurationsFromAssembly(typeof(AppDbContext).Assembly);
+        }
+    }
+}

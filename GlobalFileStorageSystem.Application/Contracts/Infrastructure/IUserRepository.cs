31efa2da09188834621ddca1009074216c37cb80
Levan Ilashvili <levanilashvili2005@gmail.com>
2025-07-16 11:02:53 +0400

    feat: create user repository

---
+++ GlobalFileStorageSystem.Application/Contracts/Infrastructure/IUserRepository.cs
@@ -0,0 +1,8 @@
+﻿using GlobalFileStorageSystem.Domain.Entities;
+
+namespace GlobalFileStorageSystem.Application.Contracts.Infrastructure
+{
+    public interface IUserRepository : IAsyncRepository<User>
+    {
+    }
+}
---
+++ GlobalFileStorageSystem.Infrastructure/Repositories/UserRepository.cs
@@ -0,0 +1,13 @@
+﻿using GlobalFileStorageSystem.Application.Contracts.Infrastructure;
+using GlobalFileStorageSystem.Domain.Entities;
+using GlobalFileStorageSystem.Infrastructure.Persistance;
+
+namespace GlobalFileStorageSystem.Infrastructure.Repositories
+{
+    public class UserRepository : BaseRepository<User>, IUserRepository
+    {
+        public UserRepository(AppDbContext dbContext) : base(dbContext)
+        {
+        }
+    }
+}

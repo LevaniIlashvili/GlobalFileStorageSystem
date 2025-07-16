e438e0b0ba5814e6a9e80ca176029c1efecd0ff5
Levan Ilashvili <levanilashvili2005@gmail.com>
2025-07-16 11:07:21 +0400

    feat: register application services

---
+++ GlobalFileStorageSystem.Application/ApplicationServiceRegistration.cs
@@ -0,0 +1,21 @@
+﻿using FluentValidation;
+using GlobalFileStorageSystem.Application.Behaviors;
+using MediatR;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace GlobalFileStorageSystem.Application
+{
+    public static class ApplicationServiceRegistration
+    {
+        public static IServiceCollection AddApplicationServices(this IServiceCollection services)
+        {
+            services.AddMediatR(cfg => cfg.RegisterServicesFromAssemblies(AppDomain.CurrentDomain.GetAssemblies()));
+
+            services.AddValidatorsFromAssemblies(AppDomain.CurrentDomain.GetAssemblies());
+
+            services.AddTransient(typeof(IPipelineBehavior<,>), typeof(ValidationBehavior<,>));
+
+            return services;
+        }
+    }
+}

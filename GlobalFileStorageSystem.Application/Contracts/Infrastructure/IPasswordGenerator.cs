33e3b2f9aeca9dad27dd16b084c3c5aa90cacc0d
Levan Ilashvili <levanilashvili2005@gmail.com>
2025-07-16 16:19:15 +0400

    feat: create password generator

---
+++ GlobalFileStorageSystem.Application/Contracts/Infrastructure/IPasswordGenerator.cs
@@ -0,0 +1,7 @@
+﻿namespace GlobalFileStorageSystem.Application.Contracts.Infrastructure
+{
+    public interface IPasswordGenerator
+    {
+        string Generate(int length = 12);
+    }
+}
---
+++ GlobalFileStorageSystem.Infrastructure/Services/PasswordGenerator.cs
@@ -0,0 +1,15 @@
+﻿using GlobalFileStorageSystem.Application.Contracts.Infrastructure;
+
+namespace GlobalFileStorageSystem.Infrastructure.Services
+{
+    public class PasswordGenerator : IPasswordGenerator
+    {
+        public string Generate(int length = 12)
+        {
+            const string valid = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890!@#$%^&*";
+            var random = new Random();
+            return new string(Enumerable.Repeat(valid, length)
+                .Select(s => s[random.Next(s.Length)]).ToArray());
+        }
+    }
+}

16eb1eeeea2c62ce2d1552e7d79276a9c452dd31
Levan Ilashvili <levanilashvili2005@gmail.com>
2025-07-16 14:44:50 +0400

    feat: register automapper

--- GlobalFileStorageSystem.Application/ApplicationServiceRegistration.cs
+++ GlobalFileStorageSystem.Application/ApplicationServiceRegistration.cs
@@ -11,6 +11,8 @@ namespace GlobalFileStorageSystem.Application
         {
             services.AddMediatR(cfg => cfg.RegisterServicesFromAssemblies(AppDomain.CurrentDomain.GetAssemblies()));
 
+            services.AddAutoMapper(AppDomain.CurrentDomain.GetAssemblies());
+
             services.AddValidatorsFromAssemblies(AppDomain.CurrentDomain.GetAssemblies());
 
             services.AddTransient(typeof(IPipelineBehavior<,>), typeof(ValidationBehavior<,>));

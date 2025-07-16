ba0be94a90e1ffd2a00c4e52df777080ef1370ab
Levan Ilashvili <levanilashvili2005@gmail.com>
2025-07-16 14:43:26 +0400

    feat: create minio options class

---
+++ GlobalFileStorageSystem.Infrastructure/Options/MinioOptions.cs
@@ -0,0 +1,9 @@
+﻿namespace GlobalFileStorageSystem.Infrastructure.Options
+{
+    public class MinioOptions
+    {
+        public string Endpoint { get; set; }
+        public string AccessKey { get; set; }
+        public string SecretKey { get; set; }
+    }
+}

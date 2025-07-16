4edb151065909e7cb2e34f5e1f96d5a951c165ff
Levan Ilashvili <levanilashvili2005@gmail.com>
2025-07-16 14:45:06 +0400

    feat: create mapping class

---
+++ GlobalFileStorageSystem.Application/Profiles/MappingProfile.cs
@@ -0,0 +1,14 @@
+﻿using AutoMapper;
+using GlobalFileStorageSystem.Application.Features.Tenants.Commands.CreateTenant;
+using GlobalFileStorageSystem.Domain.Entities;
+
+namespace GlobalFileStorageSystem.Application.Profiles
+{
+    public class MappingProfile : Profile
+    {
+        public MappingProfile()
+        {
+            CreateMap<Tenant, TenantViewmodel>().ReverseMap();
+        }
+    }
+}

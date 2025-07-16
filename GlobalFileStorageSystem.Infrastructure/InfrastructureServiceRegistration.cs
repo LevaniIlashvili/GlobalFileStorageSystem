11c19340dcdb7d04851173dc091b996a4285814c
Levan Ilashvili <levanilashvili2005@gmail.com>
2025-07-16 14:43:48 +0400

    feat: register minio client and service

--- GlobalFileStorageSystem.Infrastructure/InfrastructureServiceRegistration.cs
+++ GlobalFileStorageSystem.Infrastructure/InfrastructureServiceRegistration.cs
@@ -1,9 +1,14 @@
-﻿using GlobalFileStorageSystem.Application.Contracts.Infrastructure.Repositories;
+﻿using GlobalFileStorageSystem.Application.Contracts.Infrastructure;
+using GlobalFileStorageSystem.Application.Contracts.Infrastructure.Repositories;
+using GlobalFileStorageSystem.Infrastructure.Options;
 using GlobalFileStorageSystem.Infrastructure.Persistance;
 using GlobalFileStorageSystem.Infrastructure.Repositories;
+using GlobalFileStorageSystem.Infrastructure.Services;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Options;
+using Minio;
 
 namespace GlobalFileStorageSystem.Infrastructure
 {
@@ -15,6 +20,20 @@ namespace GlobalFileStorageSystem.Infrastructure
         {
             services.AddDbContext<AppDbContext>(options =>
                 options.UseNpgsql(configuration.GetConnectionString("PostgreSqlConnection")));
+
+            services.Configure<MinioOptions>(configuration.GetSection("Minio"));
+
+            services.AddSingleton(sp =>
+            {
+                var minioSettings = sp.GetRequiredService<IOptions<MinioOptions>>().Value;
+
+                return new MinioClient()
+                    .WithEndpoint(minioSettings.Endpoint.Replace("http://", "").Replace("https://", ""))
+                    .WithCredentials(minioSettings.AccessKey, minioSettings.SecretKey)
+                    .Build();
+            });
+
+            services.AddSingleton<IMinioService, MinioService>();
 
             services.AddScoped<ITenantRepository, TenantRepository>();
             services.AddScoped<IUserRepository, UserRepository>();

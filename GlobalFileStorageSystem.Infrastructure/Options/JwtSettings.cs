655b5ce6ece83df2355a0b49b64b468010f201c2
Levan Ilashvili <levanilashvili2005@gmail.com>
2025-07-17 18:06:03 +0400

    feat: create jwt settings class

---
+++ GlobalFileStorageSystem.Infrastructure/Options/JwtSettings.cs
@@ -0,0 +1,11 @@
+﻿namespace GlobalFileStorageSystem.Infrastructure.Options
+{
+    public class JwtSettings
+    {
+        public string Issuer { get; set; }
+        public string Audience { get; set; }
+        public string SecretKey { get; set; }
+        public int AccessTokenExpirationMinutes { get; set; }
+        public int RefreshTokenExpirationDays { get; set; }
+    }
+}

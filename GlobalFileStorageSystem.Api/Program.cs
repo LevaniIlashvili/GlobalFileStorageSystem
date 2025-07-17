a07137e70ad4cad42771873601cbe9e48c734195
Levan Ilashvili <levanilashvili2005@gmail.com>
2025-07-17 18:11:53 +0400

    feat: add jwt authentication

--- GlobalFileStorageSystem.Api/Program.cs
+++ GlobalFileStorageSystem.Api/Program.cs
@@ -1,6 +1,9 @@
 using GlobalFileStorageSystem.Api.Middlewares;
 using GlobalFileStorageSystem.Application;
 using GlobalFileStorageSystem.Infrastructure;
+using GlobalFileStorageSystem.Infrastructure.Options;
+using Microsoft.IdentityModel.Tokens;
+using System.Text;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -12,6 +15,23 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+
+var jwtSettings = builder.Configuration.GetSection("JwtSettings").Get<JwtSettings>();
+builder.Services.AddAuthentication("Bearer")
+    .AddJwtBearer("Bearer", options =>
+    {
+        options.TokenValidationParameters = new TokenValidationParameters
+        {
+            ValidateIssuer = true,
+            ValidateAudience = true,
+            ValidateLifetime = true,
+            ValidateIssuerSigningKey = true,
+            ValidIssuer = jwtSettings.Issuer,
+            ValidAudience = jwtSettings.Audience,
+            IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(jwtSettings.SecretKey))
+        };
+    });
 
 var app = builder.Build();
 

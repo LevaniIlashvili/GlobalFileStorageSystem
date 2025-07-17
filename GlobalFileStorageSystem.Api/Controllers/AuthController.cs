5cac14620c83e2da5be20d36009618daf31a2084
Levan Ilashvili <levanilashvili2005@gmail.com>
2025-07-17 18:12:11 +0400

    feat: implement login functionality

---
+++ GlobalFileStorageSystem.Api/Controllers/AuthController.cs
@@ -0,0 +1,25 @@
+﻿using GlobalFileStorageSystem.Application.Features.Auth.Queries;
+using MediatR;
+using Microsoft.AspNetCore.Mvc;
+
+namespace GlobalFileStorageSystem.Api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AuthController : ControllerBase
+    {
+        private readonly IMediator _mediator;
+
+        public AuthController(IMediator mediator)
+        {
+            _mediator = mediator;
+        }
+
+        [HttpPost("login")]
+        public async Task<ActionResult<string>> Login(LoginUserQuery query)
+        {
+            var accessToken = await _mediator.Send(query);
+            return Ok(accessToken);
+        } 
+    }
+}
---
+++ GlobalFileStorageSystem.Application/Features/Auth/Queries/LoginUserQueryHandler.cs
@@ -0,0 +1,36 @@
+﻿using GlobalFileStorageSystem.Application.Contracts.Infrastructure;
+using GlobalFileStorageSystem.Application.Contracts.Infrastructure.Repositories;
+using GlobalFileStorageSystem.Application.Exceptions;
+using MediatR;
+
+namespace GlobalFileStorageSystem.Application.Features.Auth.Queries
+{
+    public class LoginUserQueryHandler : IRequestHandler<LoginUserQuery, string>
+    {
+        private readonly IUserRepository _userRepository;
+        private readonly IPasswordHasher _passwordHasher;
+        private readonly IJwtService _jwtService;
+
+        public LoginUserQueryHandler(
+            IUserRepository userRepository,
+            IPasswordHasher passwordHasher,
+            IJwtService jwtService)
+        {
+            _userRepository = userRepository;
+            _passwordHasher = passwordHasher;
+            _jwtService = jwtService;
+        }
+
+        public async Task<string> Handle(LoginUserQuery query, CancellationToken cancellationToken)
+        {
+            var user = await _userRepository.GetByEmailAsync(query.Email, cancellationToken);
+            
+            if (user == null || !_passwordHasher.VerifyPassword(user.PasswordHash, query.Password))
+            {
+                throw new UnauthorizedException("Invalid login credentials");
+            }
+
+            return _jwtService.GenerateAccessToken(user);
+        }
+    }
+}

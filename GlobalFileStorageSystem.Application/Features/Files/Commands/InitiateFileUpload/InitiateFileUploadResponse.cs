601bb547245ab64d139b48e207f46f2da8b1e09c
Levan Ilashvili <levanilashvili2005@gmail.com>
2025-07-20 19:20:19 +0400

    feat: create InitiateFileUploadResponse class

---
+++ GlobalFileStorageSystem.Application/Features/Files/Commands/InitiateFileUpload/InitiateFileUploadResponse.cs
@@ -0,0 +1,9 @@
+﻿namespace GlobalFileStorageSystem.Application.Features.Files.Commands.InitiateFileUpload
+{
+    public class InitiateFileUploadResponse
+    {
+        public string UploadUrl { get; set; }
+        public string ObjectKey { get; set; }
+        public Guid FileId { get; set; }
+    }
+}

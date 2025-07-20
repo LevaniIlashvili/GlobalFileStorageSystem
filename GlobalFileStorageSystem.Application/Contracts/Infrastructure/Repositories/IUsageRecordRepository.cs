3d84e56a01dd4eaa4fc3354c877aab536491ca81
Levan Ilashvili <levanilashvili2005@gmail.com>
2025-07-20 19:16:06 +0400

    feat: create usagerecordrepository interface

---
+++ GlobalFileStorageSystem.Application/Contracts/Infrastructure/Repositories/IUsageRecordRepository.cs
@@ -0,0 +1,9 @@
+﻿using GlobalFileStorageSystem.Domain.Entities;
+
+namespace GlobalFileStorageSystem.Application.Contracts.Infrastructure.Repositories
+{
+    public interface IUsageRecordRepository : IAsyncRepository<UsageRecord>
+    {
+        Task<UsageRecord?> GetLatestByTenantIdAsync(Guid tenantId);
+    }
+}

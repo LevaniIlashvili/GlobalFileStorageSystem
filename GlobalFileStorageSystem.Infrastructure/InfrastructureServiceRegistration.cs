b2bfb568396ec90ad3c9b96001454f59c32b46d1
Levan Ilashvili <levanilashvili2005@gmail.com>
2025-07-20 17:28:42 +0400

    feat: register logged in user service

--- GlobalFileStorageSystem.Infrastructure/InfrastructureServiceRegistration.cs
+++ GlobalFileStorageSystem.Infrastructure/InfrastructureServiceRegistration.cs
@@ -11,6 +11,7 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Options;
 using Minio;
+using Npgsql;
 using StackExchange.Redis;
 
 namespace GlobalFileStorageSystem.Infrastructure
@@ -59,6 +60,8 @@ namespace GlobalFileStorageSystem.Infrastructure
             services.AddScoped<IUnitOfWork, UnitOfWork>();
             services.AddScoped<ITenantRepository, TenantRepository>();
             services.AddScoped<IUserRepository, UserRepository>();
+
+            services.AddScoped<ILoggedInUserService, LoggedInUserService>();
 
             return services;
         }

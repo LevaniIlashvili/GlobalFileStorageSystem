1a80a735cba426016532db3e097863d327720991
Levan Ilashvili <levanilashvili2005@gmail.com>
2025-07-20 19:21:15 +0400

    feat: create initiate file upload endpoint

---
+++ GlobalFileStorageSystem.Api/Controllers/FileController.cs
@@ -0,0 +1,27 @@
+﻿using GlobalFileStorageSystem.Application.Features.Files.Commands.InitiateFileUpload;
+using GlobalFileStorageSystem.Application.Features.Files.Commands.UploadFile;
+using MediatR;
+using Microsoft.AspNetCore.Mvc;
+
+namespace GlobalFileStorageSystem.Api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class FileController : ControllerBase
+    {
+        private readonly ISender _sender;
+
+        public FileController(ISender sender)
+        {
+            _sender = sender;
+        }
+
+        [HttpPost]
+        public async Task<ActionResult<InitiateFileUploadResponse>> InitiateFileUpload([FromBody] InitiateFileUploadCommand command)
+        {
+            var result = await _sender.Send(command);
+
+            return CreatedAtAction(null, result);
+        }
+    }
+}

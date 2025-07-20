10c9d15a22e7fd87825c07a0b88d9c9924bb1c40
Levan Ilashvili <levanilashvili2005@gmail.com>
2025-07-20 19:16:18 +0400

    feat: implement usage record repository

---
+++ GlobalFileStorageSystem.Infrastructure/Repositories/UsageRecordRepository.cs
@@ -0,0 +1,24 @@
+﻿using GlobalFileStorageSystem.Application.Contracts.Infrastructure.Repositories;
+using GlobalFileStorageSystem.Domain.Entities;
+using GlobalFileStorageSystem.Infrastructure.Persistance;
+using Microsoft.EntityFrameworkCore;
+
+namespace GlobalFileStorageSystem.Infrastructure.Repositories
+{
+    public class UsageRecordRepository : BaseRepository<UsageRecord>, IUsageRecordRepository
+    {
+        public UsageRecordRepository(AppDbContext dbContext) : base(dbContext)
+        {
+        }
+
+        public async Task<UsageRecord?> GetLatestByTenantIdAsync(Guid tenantId)
+        {
+            var usageRecord = await _dbContext.UsageRecords
+                .Where(u => u.TenantId == tenantId)
+                .OrderByDescending(u => u.SnapshotDate)
+                .FirstOrDefaultAsync();
+
+            return usageRecord;
+        }
+    }
+}

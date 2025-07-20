e2b581a9fe824a8bdf5af2158a1a21f06383c503
Levan Ilashvili <levanilashvili2005@gmail.com>
2025-07-20 19:16:48 +0400

    feat: add method for generating pre signed upload url

--- GlobalFileStorageSystem.Application/Contracts/Infrastructure/IMinioService.cs
+++ GlobalFileStorageSystem.Application/Contracts/Infrastructure/IMinioService.cs
@@ -3,6 +3,11 @@
     public interface IMinioService
     {
         Task CreateTenantBucketAsync(string tenantId);
+        Task<string> GeneratePreSignedUploadUrlAsync(
+            string tenantId,
+            string objectName,
+            string contentType,
+            TimeSpan expiration);
         Task UploadObjectAsync(string tenantId, string objectName, Stream data, string contentType);
         Task<Stream> GetObjectAsync(string tenantId, string objectName);
         Task DeleteObjectAsync(string tenantId, string objectName);

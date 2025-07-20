f13f17fa54eb91188c06d333f9f869599374e923
Levan Ilashvili <levanilashvili2005@gmail.com>
2025-07-20 19:19:20 +0400

    feat: create initiate file upload command

---
+++ GlobalFileStorageSystem.Application/Features/Files/Commands/InitiateFileUpload/InitiateFileUploadCommand.cs
@@ -0,0 +1,16 @@
+﻿using GlobalFileStorageSystem.Application.Features.Files.Commands.InitiateFileUpload;
+using GlobalFileStorageSystem.Domain.Enums;
+using MediatR;
+
+namespace GlobalFileStorageSystem.Application.Features.Files.Commands.UploadFile
+{
+    public class InitiateFileUploadCommand : IRequest<InitiateFileUploadResponse>
+    {
+        public string FileName { get; set; }
+        public long FileSize { get; set; }
+        public string ContentType { get; set; }
+        public List<string> Tags { get; set; }
+        public Dictionary<string, string> Metadata { get; set; }
+        public AccessLevel AccessLevel { get; set; }
+    }
+}

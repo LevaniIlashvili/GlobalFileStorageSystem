a394cb37512b9f98f12abd2c249689fb05c45786
Levan Ilashvili <levanilashvili2005@gmail.com>
2025-07-20 17:23:49 +0400

    feat: create logged in user service interface

---
+++ GlobalFileStorageSystem.Application/Contracts/Infrastructure/Authentication/ILoggedInUserService.cs
@@ -0,0 +1,11 @@
+﻿using GlobalFileStorageSystem.Domain.Enums;
+
+namespace GlobalFileStorageSystem.Application.Contracts.Infrastructure.Authentication
+{
+    public interface ILoggedInUserService
+    {
+        public Guid UserId { get; }
+        public string Role { get; }
+        public Permission Permissions { get; }
+    }
+}
